37974f76d619275d9554e0d6200059cfcf83c933
unknown <rugambageyannicks01@gmail.com>
2026-01-16 01:24:50 -0800

    initial commit

---
+++ CounterButton.vue
@@ -0,0 +1,33 @@
+<template>
+<button :class="buttonClass" @click="handleClick">
+<slot></slot>
+</button>
+</template>
+
+<script>
+export default {
+    name: 'CounterButton',
+    props: {
+        buttonClass: { type: String, default: 'default-button'}
+    },
+    methods: {
+        handleClick() {
+            this.$emit('clicked');
+        }
+    }
+}
+</script>
+ 
+<style>
+.default-button {
+    background-color: aqua;
+    color: white;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+.clicked-button {
+  background-color: green;  
+}
+</style>
